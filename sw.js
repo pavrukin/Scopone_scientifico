93bae3498dcf4a59f19a31a066e6c23997afe98b
pavrukin <129773262+pavrukin@users.noreply.github.com>
2025-11-11 12:52:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// sw.js
+const CACHE_NAME = 'scopone-v1';
+const FILES_TO_CACHE = [
+  './',
+  './Scopone_scientifico_app.html',
+  './manifest.json',
+  './icon_scopone_scientifico.png'
+];
+
+// Install and cache all app shell files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
